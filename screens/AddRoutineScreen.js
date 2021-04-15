939ee8170e03af5b84e84447989888217ff47e60
Max Ingelsson <maxingelsson@gmail.com>
2021-04-15 16:56:44 +0200

    custom routine fixes n shit

--- screens/AddRoutineScreen.js
+++ screens/AddRoutineScreen.js
@@ -8,18 +8,23 @@ import {
   TouchableOpacity,
   Button,
   FlatList,
+  Dimensions,
 } from 'react-native';
 import { Input } from 'react-native-elements';
 import PropTypes from 'prop-types';
 import { db, auth } from '../firebase';
 import WeekdayPicker from 'react-native-weekday-picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, Entypo, EvilIcons } from '@expo/vector-icons';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Swipeable } from 'react-native-gesture-handler';
+import { FloatingLabelInput } from 'react-native-floating-label-input';
 
 import colors from '../config/colors';
 import AppButton from '../components/AppButton';
 import Screen from '../components/Screen';
+
+const width = Dimensions.get('window').width;
 
 const AddRoutineScreen = ({ navigation }) => {
   const user = auth.currentUser;
@@ -48,20 +53,25 @@ const AddRoutineScreen = ({ navigation }) => {
     const currentDate = selectedDate || date;
     setShow(Platform.OS === 'ios');
     setDate(currentDate);
+    if (event.type == 'set') {
+      //ok button clicked
+      if (!editing) {
+        addItem(currentDate);
+      } else {
+        console.log(clickedTime);
+        setIsEditing(false);
+        //setTimes(times.find(key => key === clicked))
+        //ändra times[clicked] till currentDate
 
-    if (!editing) {
-      addItem(currentDate);
+        const newTimes = times;
+        newTimes[clickedTime].hours = currentDate.getHours();
+        newTimes[clickedTime].minutes = currentDate.getMinutes();
+        setTimes(newTimes);
+        setRefresh(!refresh);
+      }
     } else {
-      console.log(clickedTime);
-      setIsEditing(false);
-      //setTimes(times.find(key => key === clicked))
-      //ändra times[clicked] till currentDate
-
-      const newTimes = times;
-      newTimes[clickedTime - 1].hours = currentDate.getHours();
-      newTimes[clickedTime - 1].minutes = currentDate.getMinutes();
-      setTimes(newTimes);
-      setRefresh(!refresh);
+      //cancel button clicked
+      console.log('Cancel button pressed');
     }
   };
 
@@ -82,6 +92,7 @@ const AddRoutineScreen = ({ navigation }) => {
           routine: name,
           note: note,
           days: JSON.stringify(days),
+          routineTimes: JSON.stringify(times),
         })
         .then(() => {
           console.log('Document successfully written!');
@@ -97,21 +108,21 @@ const AddRoutineScreen = ({ navigation }) => {
     setDays(days);
   };
 
-  // const addTimes = (currentTimes) => {
-  //   setTimes((previousTimes) => [
-  //     ...previousTimes,
-  //     {
-  //       hours: currentTimes.getHours(),
-  //       minutes: currentTimes.getMinutes(),
-  //     },
-  //   ]);
-  // };
+  const removeTime = (clicked) => {
+    console.log('remove' + clicked);
+    setRefresh(!refresh);
+
+    const filteredData = times.filter((item, index) => index !== clicked);
+    setTimes(filteredData);
+  };
 
   const addItem = (currentDate) => {
     setTimes((previousTimes) => [
       ...previousTimes,
       {
-        key: previousTimes[previousTimes.length - 1].key + 1,
+        key: previousTimes.length
+          ? previousTimes[previousTimes.length - 1].key + 1
+          : 0,
         hours: currentDate.getHours(),
         minutes: currentDate.getMinutes(),
       },
@@ -129,46 +140,99 @@ const AddRoutineScreen = ({ navigation }) => {
   let renderItems = ({ item }) => {
     //console.log('times: ' + item);
     return (
-      <View style={styles.timeContainer}>
-        <TouchableOpacity
-          onPress={() => {
-            setIsEditing(true);
-            setClickedTime(item.key);
-            showTimepicker();
-          }}
-          style={styles.timeContainer}
-        >
-          <Text>
-            {typeof item != 'undefined'
-              ? checkNumber(item.hours) + ':' + checkNumber(item.minutes)
-              : null}{' '}
-          </Text>
-          <MaterialCommunityIcons name="cog" size={30} color={colors.samRed} />
-        </TouchableOpacity>
-      </View>
+      <Swipeable
+        friction={3}
+        renderRightActions={(progress, dragX) => {
+          const scale = dragX.interpolate({
+            inputRange: [0, 100],
+            outputRange: [0, 1],
+            extrapolate: 'clamp',
+          });
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                removeTime(times.findIndex((obj) => obj.key === item.key));
+              }}
+              style={styles.timeContainer}
+            >
+              <MaterialCommunityIcons
+                name="close"
+                size={30}
+                color={'#F45B69'}
+              />
+            </TouchableOpacity>
+          );
+        }}
+      >
+        <View style={styles.Container}>
+          <TouchableOpacity
+            onPress={() => {
+              setIsEditing(true);
+              setClickedTime(times.findIndex((obj) => obj.key === item.key));
+              showTimepicker();
+            }}
+            style={styles.timeListContainer}
+          >
+            <Text style={{ fontSize: 22, color: colors.darkmodeHighWhite }}>
+              {typeof item != 'undefined'
+                ? checkNumber(item.hours) + ':' + checkNumber(item.minutes)
+                : null}{' '}
+            </Text>
+            {/* <FontAwesome5
+              name="tune"
+              size={30}
+              color={colors.darkmodeHighWhite}
+            /> */}
+            <EvilIcons
+              name="navicon"
+              size={33}
+              color={colors.darkmodeHighWhite}
+            />
+          </TouchableOpacity>
+        </View>
+      </Swipeable>
     );
   };
 
   return (
-    <ScrollView>
-      <Screen style={styles.container}>
+    <Screen style={styles.container}>
+      <ScrollView>
         <Text style={styles.name}>Create a Custom Routine:</Text>
         <View style={styles.inputContainer}>
-          <Input
-            placeholder="Name: Morning workout"
+          <FloatingLabelInput
+            inputContainerStyle={{
+              backgroundColor: colors.darkmodeFocused,
+              borderTopLeftRadius: 6,
+              borderTopRightRadius: 6,
+            }}
+            placeholder="eg. Morning workout"
+            leftIcon={{
+              type: 'font-awesome',
+              name: 'diamond',
+              color: colors.samRed,
+            }}
             type="text"
             value={name}
             onChangeText={(text) => setName(text)}
+            style={{ color: colors.floralWhite, height: 40 }}
           />
           <Input
             placeholder="Notes (Optional): 10 pushups"
+            leftIcon={{
+              type: 'font-awesome',
+              name: 'diamond',
+              color: colors.samRed,
+            }}
             type="text"
             value={note}
             onChangeText={(text) => setNote(text)}
+            style={{ color: colors.floralWhite }}
           />
           <Text style={styles.text}>Select days:</Text>
           {pressed ? (
             <WeekdayPicker
+              dayStyle={{ backgroundColor: 'blue' }}
+              style={styles.picker}
               days={days}
               onChange={() => {
                 handleChange(days);
@@ -189,8 +253,8 @@ const AddRoutineScreen = ({ navigation }) => {
             />
           )}
 
-          <View style={styles.timeContainer}>
-            <Text>Times a day:</Text>
+          <View style={styles.timeTitleContainer}>
+            <Text style={styles.text}>Times a day:</Text>
             {show && (
               <DateTimePicker
                 testID="dateTimePicker"
@@ -210,7 +274,7 @@ const AddRoutineScreen = ({ navigation }) => {
               <MaterialCommunityIcons
                 name="plus"
                 size={30}
-                color={colors.samRed}
+                color={colors.darkmodeHighWhite}
               />
             </TouchableOpacity>
           </View>
@@ -228,21 +292,32 @@ const AddRoutineScreen = ({ navigation }) => {
             ) : null}
           </View>
         </View>
-
-        <AppButton
-          style={styles.button}
-          title="Add routine"
-          onPress={createRoutine}
-        />
-      </Screen>
-    </ScrollView>
+      </ScrollView>
+      {/* <AppButton
+        style={styles.button}
+        title="Add routine"
+        onPress={createRoutine}
+      /> */}
+      <TouchableOpacity style={styles.button} onPress={createRoutine}>
+        <Text style={styles.buttonText}>Add Routine</Text>
+      </TouchableOpacity>
+    </Screen>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    width: 200,
-    margin: 20,
+    width: width,
+    backgroundColor: '#F45B69',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  buttonText: {
+    color: colors.floralWhite,
+    fontSize: 18,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
   },
   buttonOpen: {
     backgroundColor: '#F194FF',
@@ -256,12 +331,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 22,
   },
+  container: {
+    width: width,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.darkmodeBlack,
+  },
   flatlist: {
     height: 200,
   },
   text: {
+    color: colors.darkmodeHighWhite,
     marginBottom: 20,
     textAlign: 'center',
+    fontSize: 16,
   },
   modalView: {
     margin: 20,
@@ -281,11 +365,6 @@ const styles = StyleSheet.create({
   picker: {
     marginBottom: 20,
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
 
   inputContainer: {
     width: 300,
@@ -294,14 +373,28 @@ const styles = StyleSheet.create({
 
   name: {
     fontSize: 22,
-    color: colors.samBlack,
+    color: colors.darkmodeHighWhite,
     fontWeight: '600',
     alignItems: 'center',
+  },
+  timeTitleContainer: {
+    flexDirection: 'row',
+    padding: 5,
+    justifyContent: 'center',
   },
   timeContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  timeListContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: colors.darkmodeDisabledBlack,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 50,
   },
 });
 
