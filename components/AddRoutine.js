9937021928248c3b0297f799f7ee840d1c140c5d
YungMilky <filtersome@gmail.com>
2021-04-16 11:52:19 +0200

    lindas roubies

--- components/AddRoutine.js
+++ components/AddRoutine.js
@@ -1,10 +1,17 @@
 import { auth, db } from "../firebase";
+import firebase from "firebase/app";
 
 //  Just add routine fields as props when
 //  we've decided on the logic for them
 
+const increment = firebase.firestore.FieldValue.increment(50);
+
 export default (title) => {
   const userID = auth.currentUser.uid;
+  console.log(userID);
+
+  const roubineRef = db.collection("Users").doc(userID);
+  roubineRef.update({ Roubies: increment });
 
   const userRoutines = db
     .collection("Users")
