d7819e34e8dbfb5a425e3889debe9ea789bc47b5
Timo-Dao <timothymida@gmail.com>
2021-05-06 13:30:33 +0200

    Calendar routineTimes

--- screens/JourneyScreen.js
+++ screens/JourneyScreen.js
@@ -1,27 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import { db, auth } from "../firebase";
 
-import colors from '../config/colors';
-import Screen from '../components/Screen';
+import colors from "../config/colors";
+import Screen from "../components/Screen";
 
 const JourneyScreen = ({ navigation }) => {
+  const [totalRoubies, setTotalRoubies] = useState("");
+  const user = auth.currentUser;
+
+  const getUserInfo = () => {
+    db.collection("Users")
+      .doc(user.uid)
+      .get()
+      .then((documentSnapshot) => {
+        setTotalRoubies(documentSnapshot.data().Exp);
+      });
+  };
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
   return (
     <Screen style={styles.container}>
-      <Text style={styles.text}>This page does not exist yet</Text>
+      <Text style={styles.text}>Total Roubies Earned:</Text>
+      <Text style={styles.roubieText}>{totalRoubies}</Text>
     </Screen>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
   },
   text: {
-    fontSize: 18,
+    fontSize: 20,
     color: colors.darkmodeMediumWhite,
+  },
+  roubieText: {
+    fontSize: 50,
+    color: colors.darkmodeHighWhite,
   },
 });
 
